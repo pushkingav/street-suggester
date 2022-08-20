df065267046941cb61cebbd6cbae7a1ec35e5f64
apushkin <apushkin@ipmks.com>
2022-08-21 02:14:12 +0400

    Add BlueprintJS library. Make search input more confident and listen to user input. Search is performed on Enter press.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,33 @@ import {useState} from "react";
 
 const App = () => {
     const [searchString, setSearchString] = useState("");
+    const [results, setResults] = useState([]);
 
-    const onClickHandler = async () => {
-        const response = await fetch(`http://localhost:8080/search?searchString=${searchString}`);
-        const body = await response.json();
-        console.log(body);
-    };
-    const onBlurHandler = (event) => {
+    const onChangeHandler = (event) => {
         setSearchString(event.target.value);
         console.log(event.target.value);
     };
+
+    const onKeyDownHandler = async (e) => {
+        if (e.keyCode === 13) {
+            const response = await fetch(`http://localhost:8080/search?searchString=${searchString}`);
+            const body = await response.json();
+            setResults(body);
+            console.log(results);
+        }
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <input type="search" onBlur={onBlurHandler} placeholder="Search..."/>
-                <button onClick={onClickHandler} type="submit">Search</button>
-
+                <div className="bp4-input-group .bp4-large">
+                    <span className="bp4-icon bp4-icon-search"></span>
+                    <input className="bp4-input" onChange={onChangeHandler}
+                           onKeyDown={onKeyDownHandler} type="search" placeholder="Search input" dir="auto" />
+                </div>
+                <div>
+                    {results.map((result, index) => <div key={result+index}>{result}</div>)}
+                </div>
             </header>
         </div>
     );
