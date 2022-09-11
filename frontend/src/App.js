b2716700f5248e8e68ce5b0b5e75f5af559fb38f
apushkin <apushkin@ipmks.com>
2022-09-12 01:20:45 +0400

    Add Search and Suggest buttons.
    If your Search don't find anything try the Suggest button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 const App = () => {
     const [searchString, setSearchString] = useState("");
     const [results, setResults] = useState([]);
+    const [suggestedResults, setSuggestedResults] = useState([]);
 
     const onChangeHandler = (event) => {
         setSearchString(event.target.value);
@@ -12,6 +13,7 @@ const App = () => {
 
     const onKeyDownHandler = async (e) => {
         if (e.keyCode === 13) {
+            clearResults();
             const response = await fetch(`http://localhost:8080/search?searchString=${searchString}`);
             const body = await response.json();
             setResults(body);
@@ -19,16 +21,42 @@ const App = () => {
         }
     };
 
+    const onSearchClickHandler = async () => {
+        clearResults();
+        const response = await fetch(`http://localhost:8080/search?searchString=${searchString}`);
+        const body = await response.json();
+        setResults(body);
+        console.log(results);
+    };
+
+    const onSuggestClickHandler = async () => {
+        clearResults();
+        const response = await fetch(`http://localhost:8080/search/suggest?searchString=${searchString}`);
+        const body = await response.json();
+        setSuggestedResults(body);
+        console.log(results);
+    };
+
+    const clearResults = () => {
+        setResults([]);
+        setSuggestedResults([]);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <div className="bp4-input-group .bp4-large">
-                    <span className="bp4-icon bp4-icon-search"></span>
-                    <input className="bp4-input" onChange={onChangeHandler}
-                           onKeyDown={onKeyDownHandler} type="search" placeholder="Search input" dir="auto" />
+                <div className="search-group">
+                    <div className="bp4-input-group .bp4-large">
+                        <span className="bp4-icon bp4-icon-search"></span>
+                        <input className="bp4-input" onChange={onChangeHandler}
+                               onKeyDown={onKeyDownHandler} type="search" placeholder="Search input" dir="auto"/>
+                    </div>
+                    <button onClick={onSearchClickHandler} className="bp4-button .bp4-small">Search</button>
+                    <button onClick={onSuggestClickHandler} className="bp4-button">Suggest</button>
                 </div>
                 <div>
-                    {results.map((result, index) => <div key={result+index}>{result}</div>)}
+                    {results.map((result, index) => <div key={result + index}>{result}</div>)}
+                    {suggestedResults.map((result, index) => <div key={result + index}>{result}</div>)}
                 </div>
             </header>
         </div>
