528f721b1fcfa524595958dc0969bb93c99f968e
apushkin <apushkin@ipmks.com>
2023-03-21 01:12:25 +0400

    Fix deployment docker stuff

--- search/src/main/java/com/apushkin/ssure/search/config/ElasticsearchConfiguration.java
+++ search/src/main/java/com/apushkin/ssure/search/config/ElasticsearchConfiguration.java
@@ -6,6 +6,8 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,10 +18,12 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = {"com.apushkin.ssure.search.repository"})
 @ComponentScan(basePackages = {"com.apushkin.ssure.search.model"})
 public class ElasticsearchConfiguration {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Bean
     public ElasticsearchClient restClient(@Value("${elastic.host}") String host,
                                           @Value("${elastic.port}") int port) {
+        logger.info("Elasticsearch host = {}", host);
         RestClient restClient = RestClient.builder(new HttpHost(host, port)).build();
         ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         return new ElasticsearchClient(transport);
--- search/src/main/java/com/apushkin/ssure/search/repository/ElasticSearchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.apushkin.ssure.search.repository;
-
-import com.apushkin.ssure.search.model.ElasticStreetName;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ElasticSearchRepository extends ElasticsearchRepository<ElasticStreetName, String> {
-
-}
