9eae76bcf998931eb924f27c12a17c9a0a1774be
apushkin <apushkin@ipmks.com>
2023-03-28 23:24:20 +0400

    Modify script and docker-compose.yml to adopt frontend environment variable

--- docker/compose/startup.sh
+++ docker/compose/startup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-export PUBLIC_IP=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+export REACT_APP_PUBLIC_IP=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
 && curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-ipv4)
 
-echo "Public ip = ${PUBLIC_IP}"
+echo "Public ip = ${REACT_APP_PUBLIC_IP}"
 
-# Set the path to the application.properties file
-FILEPATH="/compose/application.properties"
-
-# Find the line that starts with "public.ip.address=" and replace the IP address
-sed -i "s/^public\.ip\.address=.*/public.ip.address=$PUBLIC_IP/g" $FILEPATH
-
-echo "IP address updated successfully"
-
-#also we need to follow this guide to put it properly into the frontend container:
-#https://create-react-app.dev/docs/adding-custom-environment-variables/
+#put this script to /var/lib/cloud/scripts/per-boot/ folder of the EC2 instance
